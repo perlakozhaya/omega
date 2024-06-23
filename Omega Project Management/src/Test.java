b4474d057fc4333614aa370337ce6e33bedc7bdb
RichardAkiki <richardakiki001@gmail.com>
2024-06-23 09:41:30 +0300

    fixe perla's fault

--- Omega Project Management/src/Test.java
+++ Omega Project Management/src/Test.java
@@ -14,11 +14,11 @@ public class Test {
 		Procedure p1 = new Procedure("p1", 10, "DONE");
 		Procedure p2 = new Procedure("p2", 10, "RUNNING");
 		
-		ProcedureEmployee pe1 = new ProcedureEmployee(emp1, 6, p1);
-		ProcedureEmployee pe2 = new ProcedureEmployee(emp2, 4, p1);
+		ProcedureEmployee pe1 = new ProcedureEmployee(p1, emp1, 6);
+		ProcedureEmployee pe2 = new ProcedureEmployee(p1, emp2, 4);
 		p1.getEmployees().add(pe1);
 		p1.getEmployees().add(pe2);
-		ProcedureEmployee pe3 = new ProcedureEmployee(emp1, 4, p2);
+		ProcedureEmployee pe3 = new ProcedureEmployee(p2, emp1, 4);
 		
 		ProcedureItem pi1 = new ProcedureItem("item1", 6.0, p1);
 		ProcedureItem pi2 = new ProcedureItem("item2", 4.0, p1);
